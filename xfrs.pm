5319501215a2bd9ca2c0a9bf301841a81d972349
Live session user <xtbr123@gmail.com>
2017-10-29 20:26:40 +0100

    Functions for stock dividends and investment value.
    Added getDividend and getInvestedValue. This is a prep for a new utility
    to report protfolio performance.

--- xfrs.pm
+++ xfrs.pm
@@ -33,6 +33,36 @@ sub getSymbols {
 }
 
 
+# identifies if a symbol exists in DB
+# arguments:
+#   - reference to the open DB connection
+#   - symbol
+# returns:
+#   - true is the symbol is defined in the DB, false otherwise
+sub exists {
+    my $dbh = shift || return 0;
+    my $s = shift || return 0;
+
+    my $stmt = qq(SELECT count(*) from xfrs where );
+    # symbol used as a currency
+    $stmt = $stmt." (source_curr='$s'";
+    $stmt = $stmt." or unit_curr='$s'";
+    $stmt = $stmt." or comm_curr='$s')";
+    # end of the statement
+    $stmt = $stmt.";";
+#print "$stmt\n";
+    my $sth = $dbh->prepare( $stmt );
+    my $rv = $sth->execute();
+    if($rv < 0){
+        print $DBI::errstr;
+        return 0;
+    }
+
+    my @row = $sth->fetchrow_array();
+    return $row[0] > 0;
+}
+
+
 # gets the list of currency symbols
 # arguments:
 #   - reference to the open DB connection
@@ -273,6 +303,162 @@ sub getNAV {
         }
     }
 
+}
+
+
+# gets the total dividend (reduced by a witholding tax) for a stock symbol in the given DB
+# arguments:
+#   - reference to the open DB connection
+#   - reference to a hash array to be filled with dividend records
+sub getDividend {
+    my $dbh = shift || return;
+    my $href = shift || return;
+
+
+    my @syms = keys %$href;
+    if (scalar(@syms) == 0) {
+        @syms = getSymbols($dbh);
+    }
+
+    foreach my $s (@syms) {
+        my $dividednd = 0;
+        my $stmt; # SQL statement
+        my $sth; # compiled SQL statement handle
+        my $rv; # SQL execution return value
+
+        # get amounts that directly increase or decrease the ballance
+        $stmt = qq(select amount*unit_price - comm_price from xfrs where type='dividend' and Unit_curr = );
+        $stmt = $stmt."'$s';";
+        $sth = $dbh->prepare( $stmt );
+        $rv = $sth->execute();
+        if($rv < 0){
+            print $DBI::errstr;
+        } else {
+            while (my @row = $sth->fetchrow_array()) {
+                if (scalar(@row) < 1) { next; }
+                if (!defined $row[0]) { next; }
+                $dividend += $row[1]; }
+            }
+        }
+
+        $href->{$s} = $dividend;
+    }
+}
+
+
+# Gets the invested amount for a stock symbol in the given DB.
+#
+# The invested amount is calculated as the investment for all buy transactions
+# less the corresponding value from sell transactions. The transactions are
+# considered in stock units and the remaining number is translated into the
+# remaining invested value.
+#
+# The invested value is computed on the FIFO basis and hence the first unit bought
+# is considered the first unit sold. The remaining invested value thus represents
+# the value paid for last, yet unsold stock units.
+#
+# Commissions for transactions of yet unsold stock units increase the investment
+# value. Once all of the units of buy transaction are sold, the commision (for
+# both the buy and sell trasactions) is transfered to reduce the realized gain
+# and hence removed from the remainig invested value.
+#
+# arguments:
+#   - reference to the open DB connection
+#   - reference to a hash array to be filled with dividend records
+sub getInvestedAmount {
+    my $dbh = shift || return;
+    my $href = shift || return;
+
+
+    my @syms = keys %$href;
+    if (scalar(@syms) == 0) {
+        @syms = getSymbols($dbh);
+    }
+
+    foreach my $s (@syms) {
+        my $ballance = 0;
+        my $stmt; # SQL statement
+        my $sth; # compiled SQL statement handle
+        my $rv; # SQL execution return value
+
+        # get amounts that directly increase or decrease the ballance
+        $stmt = qq(select type, amount, unit_price, unit_curr, comm_price, comm_curr, date from xfrs where type in ('buy','sell'));
+        $stmt = $stmt." and source_curr='$s'";
+        $stmt = $stmt." order by date;";
+        $sth = $dbh->prepare( $stmt );
+        $rv = $sth->execute();
+        if($rv < 0){
+            print $DBI::errstr;
+        } else {
+            my $curr = '';
+            my @trans;
+
+            # process the transactions fetched from DB
+            while (my @row = $sth->fetchrow_array()) {
+                if (scalar(@row) < 6) { next; }
+                if (!defined $row[0] || length $row[0] == 0) { next; }
+
+                # set the currence based on the 1st transaction record
+                if ($curr eq '') { $curr = $row[3]; }
+
+                # skip the record if wrong unit currency
+                if ($curr ne $row[3]) {
+                    print "Error: Unexpected unit currency ($row[0] $row[1] $s units on $row[6]): act=$row[3], exp=$curr\n";
+                    next;
+                }
+
+                # invalidate commision if wrong currency
+                if ($curr ne $row[5]) {
+                    print "Error: Unexpected commision currency ($row[0] $row[1] $s units on $row[6]): act=$row[5], exp=$curr\n";
+                    $row[4] = 0;
+                }
+
+                # act per the transaction type
+                switch ($row[0]) {
+                    case ['sell'] {
+                        my $units = -$row[1];
+                        foreach my $rb (@trans) {
+                            $units += $rb->{'units'};
+
+                            # clear all the bought units if sold more
+                            # than in the buy `rb` transaction, else
+                            # update with what remained
+                            if ($units < 0) {
+                                $rb->{'units'} = 0;
+                            } else {
+                                $rb->{'units'} = $units;
+                                last;
+                            }
+                        }
+
+                        # sanity check:
+                        if ($units < 0) {
+                            print "Error: Selling more than bought ($row[0] $row[1] $s units on $row[6]): num=".-$units."\n";
+                        }
+                    }
+                    case ['buy'] {
+                        # add a new record into the transactions list
+                        push(@trans, (
+                                'units' => $row[1],
+                                'price' => $row[2],
+                                'curr' => $row[3],
+                                'comm' => $row[4]
+                            )
+                        );
+                    }
+                    else { print "\nError: Unknown transaction type: $row[0]\n"; }
+                }
+            }
+
+            # compute the invested value based on what has been left from
+            # buy transactions
+            foreach my $rb (@trans) {
+                $ballance += $rb->{'units'} * $rb->{'price'} + ($rb->{'units'} > 0 ? 1 : 0) * $rb->{'comm'};
+            } 
+        }
+
+        $href->{$s} = $ballance;
+    }
 }
 
 1;
